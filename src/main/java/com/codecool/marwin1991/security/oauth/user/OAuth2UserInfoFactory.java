99edb616ffe5478d935e26d2e2f171b146faf2ce
Peter <peter.zmilczak@gmail.com>
2022-11-19 15:21:12 +0100

    implemented OAuth2UserInfoFactory

--- src/main/java/com/codecool/marwin1991/security/oauth/user/OAuth2UserInfoFactory.java
+++ src/main/java/com/codecool/marwin1991/security/oauth/user/OAuth2UserInfoFactory.java
@@ -8,6 +8,10 @@ import java.util.Map;
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo get(String registrationId, Map<String, Object> attributes){
-        throw new RuntimeException("not implemented");
+        if (AuthProvider.google.name().equalsIgnoreCase(registrationId)){
+            return new GoogleOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException(registrationId + " is not supported");
+        }
     }
 }
