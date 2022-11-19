50e7b404329ddd94c05dac06016baf11ff210154
Peter <peter.zmilczak@gmail.com>
2022-11-19 15:03:04 +0100

    implemented AppUser

--- src/main/java/com/codecool/marwin1991/model/AppUser.java
+++ src/main/java/com/codecool/marwin1991/model/AppUser.java
@@ -1,15 +1,35 @@
 package com.codecool.marwin1991.model;
 
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class AppUser {
 
-    // TODO this class should be entity with fields
-    //  id; (as String with uuid2 strategy)
-    //  name;
-    //  email;
-    //  imageUrl;
-    //  emailVerified default false;
-    //  password default null;
-    //  AuthProvider provider;
-    //  String providerId;
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String imageUrl;
+
+    private Boolean emailVerified = false;
+
+    @JsonIgnore
+    private String password = null;
+
+    private AuthProvider provider;
+
+    private String providerId;
 
 }
