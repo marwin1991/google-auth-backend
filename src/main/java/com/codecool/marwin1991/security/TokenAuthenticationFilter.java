1125af1a64cfc6a05a542b452ed1b5ab9210d8ae
Peter <peter.zmilczak@gmail.com>
2022-11-19 15:47:50 +0100

    implemented TokenAuthenticationFilter

--- src/main/java/com/codecool/marwin1991/security/TokenAuthenticationFilter.java
+++ src/main/java/com/codecool/marwin1991/security/TokenAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -28,14 +29,33 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //TODO
+        try {
+
+            String jwt = getJwtFromReqest(request);
+
+            if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){
+                String userId = tokenProvider.getUserIdFromToken(jwt);
+
+                UserDetails userDetails = userService.loadUserById(userId);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        } catch (Exception e) {
+            log.error("Cloud not authenticate user", e);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String getJwtFromReqest(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
-        if(token.startsWith("Bearer ")){
+        if (token.startsWith("Bearer ")) {
             return token.substring(7);
         }
         return "";
